edb0ccddae3b376c526099ba945198225983d0b2
gizem <gizemsacal@gmail.com>
2021-05-07 17:13:31 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.EnumSet;
+
+class Main {
+
+    enum Size {
+        SMALL, MEDIUM, LARGE, EXTRALARGE
+    }
+
+    public static void main(String[] args) {
+
+        // Creating an EnumSet using range()
+        EnumSet<Size> sizes = EnumSet.range(Size.MEDIUM, Size.EXTRALARGE);
+
+        System.out.println("EnumSet: " + sizes);
+    }
+}
